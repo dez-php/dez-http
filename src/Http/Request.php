3f856e6579e84ded6b31392b5e1819e0aeb7bb90
Ivan Gontarenko <g.ivan@dizboard.com>
2015-09-21 12:59:56 +0300

    add few methods

--- src/Http/Request.php
+++ src/Http/Request.php
@@ -6,24 +6,22 @@
 
     class Request implements RequestInterface {
 
-        public function getServerHttp( $key = null, $default = null ) {
-            return $this->getServer( "http_$key", $default );
-        }
 
-        public function getServer( $key = null, $default = null ) {
-            return $this->getFromArray( $_SERVER, strtoupper( $key ), $default );
-        }
 
         public function requestMethod() {
             return strtoupper( $this->getServer( 'request_method' ) );
         }
 
-        public function isPost() {
-            return $this->requestMethod() === 'POST';
+        public function has( $key ) {
+            return isset( $_REQUEST[ $key ] );
         }
 
-        public function isAjax() {
-            return $this->getServerHttp( 'X_REQUESTED_WITH' ) === 'XMLHttpRequest';
+        public function hasQuery( $key ) {
+            return isset( $_GET[ $key ] );
+        }
+
+        public function hasPost( $key ) {
+            return isset( $_POST[ $key ] );
         }
 
         public function getRawBody() {
@@ -45,8 +43,8 @@
             return $this->getFromArray( $_GET, $key, $default );
         }
 
-        public function getFromArray( array $source, $key, $default = null ) {
-            return isset( $source[ $key ] ) ? $source[ $key ] : $default;
+        public function getServer( $key = null, $default = null ) {
+            return $this->getFromArray( $_SERVER, strtoupper( $key ), $default );
         }
 
         public function getServerIP() {
@@ -75,5 +73,63 @@
 
             return $remoteAddr;
         }
+
+        public function getUserAgent() {
+            return $this->getServerHttp( 'user_agent' );
+        }
+
+        public function getSchema() {
+            $schema = $this->getServer( 'https' );
+            return ! $schema || $schema === 'off' ? 'http' : 'https';
+        }
+
+        public function getFromArray( array $source, $key, $default = null ) {
+            return isset( $source[ $key ] ) ? $source[ $key ] : $default;
+        }
+
+        public function getServerHttp( $key = null, $default = null ) {
+            return $this->getServer( "http_$key", $default );
+        }
+
+        public function isMethod( array $methods ) {
+            return in_array( $this->requestMethod(), $methods );
+        }
+
+        public function isAjax() {
+            return $this->getServerHttp( 'X_REQUESTED_WITH' ) === 'XMLHttpRequest';
+        }
+
+        public function isGet() {
+            return $this->requestMethod() === 'GET';
+        }
+
+        public function isPost() {
+            return $this->requestMethod() === 'POST';
+        }
+
+        public function isPut() {
+            return $this->requestMethod() === 'PUT';
+        }
+
+        public function isDelete() {
+            return $this->requestMethod() === 'DELETE';
+        }
+
+        public function isPatch() {
+            return $this->requestMethod() === 'PATCH';
+        }
+
+        public function isHead() {
+            return $this->requestMethod() === 'HEAD';
+        }
+
+        public function isOptions() {
+            return $this->requestMethod() === 'OPTIONS';
+        }
+
+        public function isSecure() {
+            return $this->getSchema() === 'https';
+        }
+
 
     }
--- src/Http/Request/RequestInterface.php
+++ src/Http/Request/RequestInterface.php
@@ -4,12 +4,57 @@
 
     interface RequestInterface {
 
-        public function get();
 
-        public function getPost();
+        public function requestMethod();
+
+        public function has( $key );
+
+        public function hasQuery( $key );
+
+        public function hasPost( $key );
+
+        public function getRawBody();
+
+        public function get( $key, $default );
+
+        public function getPost( $key, $default );
+
+        public function getQuery( $key, $default );
+
+        public function getServerHttp();
+
+        public function getServer();
+
+        public function getServerIP();
+
+        public function getClientIP();
+
+        public function getRealClientIP();
+
+        public function getUserAgent();
+
+        public function getSchema();
+
+        public function getFromArray( array $source, $key, $default );
+
+        public function isMethod( array $methods );
+
+        public function isAjax();
+
+        public function isGet();
 
         public function isPost();
 
-        public function isAjax();
+        public function isPut();
+
+        public function isDelete();
+
+        public function isPatch();
+
+        public function isHead();
+
+        public function isOptions();
+
+        public function isSecure();
 
     }
